3df0e1f1a4b5a33d732c8d066f93b2b25f5a39ac
andisfar <andisfar@hotmail.com>
2017-12-25 12:21:30 -0500

    Still Implementing DAL.  Debugging init problems

--- TimeTrackerDataAccessLayer/NeedConnectionStringEventArgs.cs
+++ TimeTrackerDataAccessLayer/NeedConnectionStringEventArgs.cs
@@ -5,7 +5,7 @@ namespace TimeTrackerDataAccessLayer
     public class NeedConnectionStringEventArgs : EventArgs, IDisposable
     {
         string connection_string;
-        public string ConnectionString {get => connection_string;}
+        public string ConnectionString { get => connection_string; set => connection_string = value; }
         public NeedConnectionStringEventArgs(string connectionstring)
         {
             connection_string = connectionstring;
@@ -13,6 +13,7 @@ namespace TimeTrackerDataAccessLayer
         public void Dispose()
         {
             connection_string = string.Empty;
+            GC.SuppressFinalize(this);
         }
     }
 }
