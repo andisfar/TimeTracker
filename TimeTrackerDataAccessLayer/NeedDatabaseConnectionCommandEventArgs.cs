1eaff31c51c36340f1c17d94e4a45978d9e4ccba
andisfar <andisfar@hotmail.com>
2017-12-25 12:21:30 -0500

    Still Implementing DAL.  Debugging init problems

---
+++ TimeTrackerDataAccessLayer/NeedDatabaseConnectionCommandEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.SQLite;
+
+namespace TimeTrackerDataAccessLayer
+{
+    public class NeedDatabaseConnectionCommandEventArgs : EventArgs, IDisposable
+    {
+        SQLiteCommand _create_db_command;
+        public SQLiteCommand CreateDatabase { get => _create_db_command; set => _create_db_command = value; }
+        public NeedDatabaseConnectionCommandEventArgs() : base()
+        { }
+        public void Dispose()
+        {
+            _create_db_command.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
